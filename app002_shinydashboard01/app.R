9571cefe6eaad6e2420a50142520ff0929f7c4e9
Dag_Hjermann <dhj@niva.no>
2022-03-09 09:28:15 +0100

    Add (not-working) button
    Also changed name of tabs, that is why it looks like a lot has changed

--- app002_shinydashboard01/app.R
+++ app002_shinydashboard01/app.R
@@ -2,6 +2,9 @@
 # Basic shinydashboard from
 # https://rstudio.github.io/shinydashboard/get_started.html
 
+# Tried to add button to change tab, that didn't work
+# (loosely following https://mastering-shiny.org/action-dynamic.html#dynamic-wizard) 
+
 # app.R ##
 library(shiny)
 library(shinydashboard)
@@ -10,35 +13,46 @@ ui <- dashboardPage(
   dashboardHeader(title = "Basic dashboard"),
   dashboardSidebar(
     sidebarMenu(
-      menuItem("Dashboard", tabName = "dashboard", icon = icon("dashboard")),
-      menuItem("Widgets", tabName = "widgets", icon = icon("th"))
+      menuItem("Dashboard", tabName = "page_1", icon = icon("dashboard")),
+      menuItem("Widgets", tabName = "page_2", icon = icon("th"))
     )
   ),
   dashboardBody(
     tabItems(
       # First tab content
-      tabItem(tabName = "dashboard",
-              fluidRow(
-                box(plotOutput("plot1", height = 250)),
-                
-                box(
-                  title = "Controls",
-                  sliderInput("slider", "Number of observations:", 1, 100, 50)
-                )
-              )
+      tabItem(
+        tabName = "page_1",
+        fluidRow(
+          box(plotOutput("plot1", height = 250)),
+          box(
+            title = "Controls",
+            sliderInput("slider", "Number of observations:", 1, 100, 50)
+          )
+        ),
+        fluidRow(
+          actionButton("page_12", "next")
+        )
       ),
       
       # Second tab content
-      tabItem(tabName = "widgets",
-              h2("Widgets tab content")
+      tabItem(
+        tabName = "page_2",
+        h2("Widgets tab content")
       )
     )
   )
 )
-  
+
 
 server <- function(input, output) {
   set.seed(122)
+
+  switch_page <- function(i) {
+    updateTabsetPanel(inputId = "wizard", selected = paste0("page_", i))
+  }
+  
+  observeEvent(input$page_12, switch_page(2))
+  
   histdata <- rnorm(500)
   
   output$plot1 <- renderPlot({
