531110b011b1ac1eaf943be7115077125882b08b
Dag_Hjermann <dhj@niva.no>
2025-01-24 15:22:46 +0100

    app201: click station on map, plot data

---
+++ app201_mapdata/app.R
@@ -0,0 +1,70 @@
+library(shiny)
+library(leaflet)
+library(dplyr)
+library(ggplot2)
+
+# Read data
+data_all <- read.csv("../input_data/milkys_example.csv")
+stations <- read.csv("../input_data/milkys_example_coord.csv") %>%
+  rename(x = Long, y = Lat)  %>%
+  filter(STATION_CODE %in% unique(data_all$STATION_CODE))
+params_all <- unique(data_all$PARAM) %>% sort()
+
+
+# from
+# https://www.r-bloggers.com/2017/03/4-tricks-for-working-with-r-leaflet-and-shiny/
+
+ui <- fluidPage(
+  br(),
+  column(8,
+         leafletOutput("map", height="600px")),
+  column(4, 
+         br(),br(),br(),br(),
+         shiny::selectInput("param", "Substance", params_all, "CD"),
+         plotOutput("plot", height="300px")),
+  br()
+)
+
+server <- function(input, output) {
+  
+  # build data with 2 places
+  # stations = data.frame(x=c(130, 128), y=c(-22,-26), id=c("place1", "place2"))
+  
+  # create a reactive value that will store the click position
+  data_of_click <- reactiveValues(clickedMarker=NULL)
+  
+  # Leaflet map with 2 markers
+  output$map <- renderLeaflet({
+    leaflet() %>% 
+      setView(lng=11 , lat =60, zoom=6) %>%
+      addTiles(options = providerTileOptions(noWrap = TRUE)) %>%
+      addCircleMarkers(data=stations, ~x , ~y, layerId=~STATION_CODE, popup=~STATION_CODE, radius=8 , color="black",  fillColor="red", stroke = TRUE, fillOpacity = 0.8)
+  })
+  # store the click
+  observeEvent(input$map_marker_click,{
+    data_of_click$clickedMarker <- input$map_marker_click
+  })
+  
+  # Make a barplot or scatterplot depending of the selected point
+  output$plot=renderPlot({
+    selected_station = data_of_click$clickedMarker$id
+    if(is.null(selected_station)){selected_station = "30B"}
+    # print(selected_station)
+    # print(data_of_click$clickedMarker)
+    data_selected <- data_all %>% 
+      filter(STATION_CODE %in% selected_station & PARAM %in% input$param)
+    # plot(concentration~MYEAR, data = data_selected, main = paste("Station", selected_station))
+    ggplot(data_selected, aes(x = MYEAR, y = concentration)) +
+      geom_line() +
+      geom_point() +
+      theme_minimal() +
+      labs(
+        title = paste("Measurements for station:", selected_station),
+        x = "Year",
+        y = "Concentration"
+      )
+  })
+}
+
+shinyApp(ui = ui, server = server)
+
